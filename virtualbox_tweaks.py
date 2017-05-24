4b432027fbefcab2fdfffc27989093449e37149f
Carlos Ortigoza Dempster <carlos.otgz@gmail.com>
2017-05-24 12:16:58 +0200

    Importing missing library and creating missing objects

--- virtualbox_tweaks.py
+++ virtualbox_tweaks.py
@@ -3,11 +3,15 @@
 import os
 import shutil
 import sys
+import ConfigParser
 
-# These are some Virtualbox workarounds and checks
 virtualbox_cfg = ConfigParser.ConfigParser()
 virtualbox_cfg.read("/cuckoo/conf/virtualbox.conf")
 
+cuckoo_cfg = ConfigParser.ConfigParser()
+cuckoo_cfg.read("/cuckoo/conf/cuckoo.conf")
+
+# These are some Virtualbox workarounds and checks
 if cuckoo_cfg.get('cuckoo', 'machinery') == 'virtualbox':
     if os.path.exists('/key'):
         shutil.copyfile('/key', '/cuckoo/key')
